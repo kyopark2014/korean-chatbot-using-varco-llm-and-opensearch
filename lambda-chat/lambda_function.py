81f347903db2f80adbde73a13ed0255fd2074d17
ksdyb.park <ksdyb@amazon.com>
2023-08-26 08:09:09 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -225,7 +225,7 @@ def test_embedding():
     payload = {"text_inputs": [text1, text2, text3]}
 
     client = boto3.client('runtime.sagemaker')
-    query_response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=payload).encode('utf-8'))
+    query_response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=payload).encode('utf-8')
 
     model_predictions = json.loads(query_response['Body'].read())
     embeddings = model_predictions['embedding']

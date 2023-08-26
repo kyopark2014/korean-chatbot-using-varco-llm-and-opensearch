83152656cb1f20ba8d72c2c2b286cf94ed2f929a
ksdyb.park <ksdyb@amazon.com>
2023-08-26 20:37:34 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -175,13 +175,11 @@ def get_answer_using_template(query, vectorstore):
     
     #print('length of relevant_documents: ', len(relevant_documents))
     
-    #prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    prompt_template = """Human: 다음의 내용을 확인후 질문에 정확히 답을 해줘. 만약 모른다면 모른다고 할것.
-
+    prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
     {context}
 
-    질문: {question}
+    Question: {question}
     Assistant:"""
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
